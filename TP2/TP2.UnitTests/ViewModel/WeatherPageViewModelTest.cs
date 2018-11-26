6f6b797daccd9d1b382b0f3f0082e32dd36ed61a
unknown <jenbenrossignol@hotmail.com>
2018-11-26 08:39:23 -0500

    ajout des tests pour verifier les propriety change

--- TP2/TP2.UnitTests/ViewModel/WeatherPageViewModelTest.cs
+++ TP2/TP2.UnitTests/ViewModel/WeatherPageViewModelTest.cs
@@ -15,5 +15,43 @@ namespace TP2.UnitTests.ViewModel
     public class WeatherPageViewModelTest
     {
 
+        public Mock<IPageDialogService> _pageDialogServiceMock;
+        public Mock<INavigationService> _navigationServiceMock;
+        public WeatherPageViewModel _weatherPageViewModel;
+
+        public bool _eventRaised = false;
+        public RootObject _rootObject;
+
+        public WeatherPageViewModelTest()
+        {
+            _pageDialogServiceMock = new Mock<IPageDialogService>();
+            _navigationServiceMock = new Mock<INavigationService>();
+            _weatherPageViewModel = new WeatherPageViewModel(_navigationServiceMock.Object, _pageDialogServiceMock.Object);
+        }
+
+        [Fact]
+        public void Region_WhenSetNewValue_ShouldRaisePropertyChangedEvent()
+        {
+            _weatherPageViewModel.PropertyChanged += RaiseProperty;
+
+            _weatherPageViewModel.Region = "quebec";
+
+            Assert.True(_eventRaised);
+        }
+
+        [Fact]
+        public void WeatherCondition_WhenSetNewValue_ShouldRaisePropertyChangedEvent()
+        {
+            _weatherPageViewModel.PropertyChanged += RaiseProperty;
+
+            _weatherPageViewModel.WeatherCondition = new RootObject();
+
+            Assert.True(_eventRaised);
+        }
+
+        private void RaiseProperty(object sender, PropertyChangedEventArgs e)
+        {
+            _eventRaised = true;
+        }
     }
 }
