e3375764798d19e3f52e31c7ee1b6adff3f91909
unknown <jenbenrossignol@hotmail.com>
2018-11-29 12:20:54 -0500

    ajout de la classe pour la regle de validation pour les lettres majuscules

--- TP2/TP2/TP2/Validations/Rules/HasAtleastOneCapCharacter.cs
+++ TP2/TP2/TP2/Validations/Rules/HasAtleastOneCapCharacter.cs
@@ -1,4 +1,5 @@
-﻿using TP2.Validations.Interface;
+﻿using System.Linq;
+using TP2.Validations.Interface;
 
 namespace TP2.Validations.Rules
 {
@@ -8,7 +9,9 @@ namespace TP2.Validations.Rules
 
         public bool Check(T value)
         {
-            throw new System.NotImplementedException();
+            if (value == null) return false;
+
+            return value.ToString().Any(char.IsUpper);
         }
     }
 }
