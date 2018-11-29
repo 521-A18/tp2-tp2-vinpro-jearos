3f27595fe94b9c730b4b1fc8a28e2b944fafc0da
unknown <jenbenrossignol@hotmail.com>
2018-11-29 11:21:14 -0500

    ajout de la regle de validation pour un mot de passe de 10 characters ou plus

---
+++ TP2/TP2/TP2/Validations/Rules/HasMoreThanTenCharacters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TP2.Validations.Interface;
+
+namespace TP2.Validations.Rules
+{
+    public class HasMoreThanTenCharacters<T> : IValidationRule<T> 
+    {
+        public string ErrorMessage { get; set; }
+        
+        public bool Check(T value)
+        {
+            if (value == null) return false;
+
+            string valueInString = value.ToString();
+
+            if (valueInString.Length < 10) return false;
+
+            return true;
+        }
+    }
+}
