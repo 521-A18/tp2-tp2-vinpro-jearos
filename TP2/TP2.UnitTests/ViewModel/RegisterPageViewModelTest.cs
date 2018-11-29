b1c684c52b8102160d8a83cf67d8a4c422a0c3cd
unknown <jenbenrossignol@hotmail.com>
2018-11-29 12:52:15 -0500

    ajout des tests pour les RaisedPropertyChanged

---
+++ TP2/TP2.UnitTests/ViewModel/RegisterPageViewModelTest.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using Moq;
+using Prism.Navigation;
+using TP2.Validations;
+using TP2.ViewModels;
+using Xunit;
+
+namespace TP2.UnitTests.ViewModel
+{
+    public class RegisterPageViewModelTest
+    {
+        private readonly RegisterPageViewModel _registerPageViewModels;
+        private Mock<INavigationService> _mockNavigationService;
+        private bool _eventRaised = false;
+
+        public RegisterPageViewModelTest()
+        {
+            _mockNavigationService = new Mock<INavigationService>();
+            _registerPageViewModels = new RegisterPageViewModel(_mockNavigationService.Object);
+        }
+
+        [Fact]
+        public void Email_WhenSetToNewValue_ShouldRaisePropertyChangedEvent()
+        {
+            _registerPageViewModels.PropertyChanged += RaiseProperty;
+
+            _registerPageViewModels.Email = new ValidatableObject<string>();
+
+            Assert.True(_eventRaised);
+        }
+
+        [Fact]
+        public void Password_WhenSetToNewValue_ShouldRaisePropertyChangedEvent()
+        {
+            _registerPageViewModels.PropertyChanged += RaiseProperty;
+
+            _registerPageViewModels.Password = new ValidatableObject<string>();
+
+            Assert.True(_eventRaised);
+        }
+
+        [Fact]
+        public void PasswordConfirm_WhenSetToNewValue_ShouldRaisePropertyChangedEvent()
+        {
+            _registerPageViewModels.PropertyChanged += RaiseProperty;
+
+            _registerPageViewModels.PasswordConfirm = new ValidatableObject<string>();
+
+            Assert.True(_eventRaised);
+        }
+
+        private void RaiseProperty(object sender, PropertyChangedEventArgs e)
+        {
+            _eventRaised = true;
+        }
+    }
+}
--- TP2/TP2/TP2/ViewModels/RegisterPageViewModel.cs
+++ TP2/TP2/TP2/ViewModels/RegisterPageViewModel.cs
@@ -1,12 +1,54 @@
 ﻿using Prism.Mvvm;
+using Prism.Navigation;
+using TP2.Validations;
 
 namespace TP2.ViewModels
 {
-    public class RegisterPageViewModel : BindableBase
+    public class RegisterPageViewModel : ViewModelBase
     {
-        public RegisterPageViewModel()
-        {
+        private readonly INavigationService _navigationService;
 
+        private ValidatableObject<string> _email;
+        private ValidatableObject<string> _password;
+        private ValidatableObject<string> _passwordConfirm;
+
+        public RegisterPageViewModel(INavigationService navigationService)
+            :base(navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public ValidatableObject<string> Email
+        {
+            get; set;
+            //get => _email;
+            //set 
+            //{
+            //    _email = value;
+            //    RaisePropertyChanged();
+            //}
+        }
+
+        public ValidatableObject<string> Password
+        {
+            get; set;
+            //get => _password;
+            //set
+            //{
+            //    _password = value;
+            //    RaisePropertyChanged();
+            //}
+        }
+
+        public ValidatableObject<string> PasswordConfirm
+        {
+            get; set;
+            //get => _passwordConfirm;
+            //set
+            //{
+            //    _passwordConfirm = value;
+            //    RaisePropertyChanged();
+            //}
         }
     }
 }
